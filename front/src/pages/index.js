ca7f8ad14c41e23716f127b45c3e958a66424f76
Malick NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-03-30 00:11:46 +0200

    footer

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,9 +1,11 @@
 import Title from '@/components/UI/Title';
+import Footer from '@/components/partials/Footer';
 
 export default function Home() {
   return (
     <>
       <Title title="Homepage" Level="h1" />
+      <Footer />
     </>
   );
 }
