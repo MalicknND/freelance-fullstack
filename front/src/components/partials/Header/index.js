5bb8aedd9f1faad0d5b8583b792f12048b0ab883
Malick NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-04-01 03:34:55 +0200

    navbar

--- front/src/components/partials/Header/index.js
+++ front/src/components/partials/Header/index.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './index.module.scss';
+import Button from '@/components/UI/Button/';
 
 const index = () => {
-  return <div>index</div>;
+  const [active, setActive] = useState(false);
+  return (
+    <div className={active ? `${styles.wrapperActive}` : `${styles.wrapper}`}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <span className={styles.text}>Fiverr</span>
+          <span className={styles.dot}>.</span>
+        </div>
+        <div className={styles.links}>
+          <span>Qui sommes-nous ?</span>
+          <span>Explore</span>
+          <span>English</span>
+          <Button type="submit" title="Se connecter" className="btn__primary" />
+          <Button type="submit" title="S'inscrire" className="btn__primary" />
+        </div>
+      </div>
+
+      <hr />
+      <div className={styles.menu}>
+        <span>TEST</span>
+        <span>TEST</span>
+      </div>
+    </div>
+  );
 };
 
 export default index;
--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,9 +1,11 @@
 import Title from '@/components/UI/Title';
 import Footer from '@/components/partials/Footer';
+import Header from '@/components/partials/Header';
 
 export default function Home() {
   return (
     <>
+      <Header />
       <Title title="Homepage" Level="h1" />
       <Footer />
     </>
