31b1e0ee4c9ed125de3a5c6276dcbdfbef763cf5
Malick NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-04-25 12:21:25 +0200

    login & UseContext

--- front/src/pages/_app.js
+++ front/src/pages/_app.js
@@ -2,24 +2,24 @@ import '@/styles/globals.scss';
 import { useRouter } from 'next/router';
 import MainLayout from '@/components/layouts/MainLayout';
 import AuthLayout from '@/components/layouts/AuthLayout';
-// import { UserContextProvider } from '@/context/UserContext';
+import { UserContextProvider } from '@/context/UserContext';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   return (
     <>
-      {/* <UserContextProvider> */}
-      {router.asPath.startsWith('/auth') ? (
-        <AuthLayout>
-          <Component {...pageProps} />
-        </AuthLayout>
-      ) : (
-        <MainLayout>
-          <Component {...pageProps} />
-        </MainLayout>
-      )}
-      {/* </UserContextProvider> */}
+      <UserContextProvider>
+        {router.asPath.startsWith('/auth') ? (
+          <AuthLayout>
+            <Component {...pageProps} />
+          </AuthLayout>
+        ) : (
+          <MainLayout>
+            <Component {...pageProps} />
+          </MainLayout>
+        )}
+      </UserContextProvider>
     </>
   );
 }
---
+++ front/src/pages/account/profil/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from '@/components/UI/Title';
+
+const index = () => {
+  return (
+    <div>
+      <Title title="Connexion" Level="h1" />
+    </div>
+  );
+};
+
+export default index;
