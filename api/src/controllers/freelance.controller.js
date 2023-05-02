b56f42daecbde870dee82f5fe52a9c7a4327ca89
Malick NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-05-02 22:17:59 +0200

    populate activity freelance/search

--- api/src/controllers/freelance.controller.js
+++ api/src/controllers/freelance.controller.js
@@ -1,23 +1,22 @@
-const Freelance = require("../models/freelance.model");
+const Freelance = require('../models/freelance.model');
 
 //find one freelance based in ID
 exports.findFreelance = async (req, res, next) => {
   try {
     const freelance = await Freelance.findById(req.params.id);
     if (!freelance) {
-      const error = new Error("freelance not found")
-      error.status = 404
+      const error = new Error('freelance not found');
+      error.status = 404;
       throw error;
     }
     res.send({
       success: true,
-      freelance: freelance
-    })
-  }
-  catch (err) {
+      freelance: freelance,
+    });
+  } catch (err) {
     next(err);
   }
-}
+};
 
 //find freelances based one multiple filters
 exports.findFreelances = async (req, res, next) => {
@@ -29,32 +28,32 @@ exports.findFreelances = async (req, res, next) => {
       //apply filters
       const filteredFreelance = freelances.filter((freelance) => {
         return req.body.filters.rate
-          ? freelance.rate >= req.body.filters.rate.range[0]
-          && freelance.rate <= req.body.filters.rate.range[1] : false
-          || req.body.filters.exp
-          ? freelance.yearOfExperience >= req.body.filters.exp.range[0]
-          && freelance.yearOfExperience <= req.body.filters.exp.range[1] : false
-          || req.body.filters.skills
-          ? freelance.skills.some((el) => req.body.filters.skills.includes(String(el))) : false
+          ? freelance.rate >= req.body.filters.rate.range[0] &&
+              freelance.rate <= req.body.filters.rate.range[1]
+          : false || req.body.filters.exp
+          ? freelance.yearOfExperience >= req.body.filters.exp.range[0] &&
+            freelance.yearOfExperience <= req.body.filters.exp.range[1]
+          : false || req.body.filters.skills
+          ? freelance.skills.some((el) =>
+              req.body.filters.skills.includes(String(el))
+            )
+          : false;
       });
       //return filtered freelances
       return res.send({
         success: true,
-        freelances: filteredFreelance
-      })
+        freelances: filteredFreelance,
+      });
     }
     //return non filtered freelances
     return res.send({
       success: true,
-      freelances: freelances
-    })
-  }
-  catch (err) {
+      freelances: freelances,
+    });
+  } catch (err) {
     next(err);
   }
-
-}
-
+};
 
 //find freelances based one search string
 exports.findSearchString = async (req, res, next) => {
@@ -62,42 +61,45 @@ exports.findSearchString = async (req, res, next) => {
     //find freelances
     const freelances = await Freelance.find().populate([
       {
-        path: "user",
-        model: "User"
+        path: 'user',
+        model: 'User',
       },
       {
-        path: "skills",
-        model: "Skill"
-      }
+        path: 'skills',
+        model: 'Skill',
+      },
+      {
+        path: 'activity',
+        model: 'Activity',
+      },
     ]);
     //if search string inside req.body
     if (req.body.searchString) {
-      //transform string in array bases on spaced 
+      //transform string in array bases on spaced
       const termsArray = req.body.searchString.toLowerCase().split(' ');
       //apply filter
       const filteredFreelance = freelances.filter((freelance) => {
-        return termsArray.some(
-          el => {
-            return freelance.user.firstName.includes(el) ||
-              freelance.user.lastName.includes(el) ||
-              freelance.user.address.city.includes(el) ||
-              freelance.skills.some(skill => skill.name.includes(el))
-          }
-        )
-      })
+        return termsArray.some((el) => {
+          return (
+            freelance.user.firstName.includes(el) ||
+            freelance.user.lastName.includes(el) ||
+            freelance.user.address.city.includes(el) ||
+            freelance.skills.some((skill) => skill.name.includes(el))
+          );
+        });
+      });
       // return filtered freelance
       return res.send({
         success: true,
-        freelances: filteredFreelance
-      })
+        freelances: filteredFreelance,
+      });
     }
     //return non filtered freelance
     return res.send({
       success: true,
-      freelances: freelances
-    })
-  }
-  catch (err) {
+      freelances: freelances,
+    });
+  } catch (err) {
     next(err);
   }
-}
+};
