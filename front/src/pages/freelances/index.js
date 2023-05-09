ef94850e992cd7e6616cf0065155bf86c8dbd728
Malick NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-05-09 14:06:01 +0200

    search qu'avec firstName

--- front/src/pages/freelances/index.js
+++ front/src/pages/freelances/index.js
@@ -6,23 +6,30 @@ import Title from '@/components/UI/Title';
 import Card from '@/components/UI/Card';
 import Footer from '@/components/partials/Footer';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const index = () => {
   const [data, setData] = React.useState([]);
+  const [name, setName] = React.useState('');
+  const router = useRouter();
 
   React.useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/freelance/search`, {
-      headers: {
-        'Content-Type': 'Application/json',
-      },
-      body: JSON.stringify({
-        searchString: '',
-      }),
-      method: 'POST',
-    })
-      .then((response) => response.json())
-      .then((data) => setData(data.freelances));
-  }, []);
+    if (router.isReady) {
+      console.log(router.query);
+
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/freelance/search`, {
+        headers: {
+          'Content-Type': 'Application/json',
+        },
+        body: JSON.stringify({
+          searchString: router.query.firstName,
+        }),
+        method: 'POST',
+      })
+        .then((response) => response.json())
+        .then((data) => setData(data.freelances));
+    }
+  }, [router.isReady]);
 
   return (
     <div className={styles.search}>
@@ -33,13 +40,19 @@ const index = () => {
               <img src="./img/search.png" alt="" />
               <Input
                 type="text"
-                name="email"
+                name="firstName"
                 placeholder="Développeur Web..."
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
             <div className={styles.input}>
               <img src="./img/map.png" alt="" />
-              <Input type="text" name="email" placeholder="Paris, France..." />
+              <Input
+                type="text"
+                name="address"
+                placeholder="Paris, France..."
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
           </div>
           <Button type="submit" title="Trouver" className="btn__search" />
@@ -49,7 +62,6 @@ const index = () => {
       </div>
       <Title title="Les dernières offres" Level="h1" />
       <div className={styles.freelances}>
-        {/* <div className={styles.left}>LEFT PART</div> */}
         <div className={styles.right}>
           <Link href="/freelances/id">
             <div className={styles.right_body}>
